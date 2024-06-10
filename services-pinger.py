386d8adabfd1a490967ad59893c4f4fa0fa03e01
WalkerPM <minefirst007@gmail.com>
2024-06-10 13:23:49 +0500

    first commit

---
+++ services-pinger.py
@@ -0,0 +1,146 @@
+import yaml
+import telebot
+import requests
+import logging
+import time
+from telegram_text import PlainText
+from dotenv import load_dotenv
+
+CHAT_ID =""
+BOT_TOKEN = ""
+
+from dotenv import load_dotenv
+CONFIG_FILENAME = "service-list.yaml"
+
+logging.basicConfig(
+    level=logging.INFO, format="[%(asctime)s: %(levelname)s] %(message)s"
+)
+
+class Service:
+    def __init__(self, name, address, protocol, port: str = "0000", route: str = "/") -> None:
+        self.name: str = name
+        self.address: str = address
+        self.protocol: str = protocol
+        self.port: str = port
+        self.route: str = route
+        self.status: bool = True
+        self.notified: bool = False
+        pass
+
+    def change_status(self):
+        if (self.status):
+            self.status = False
+        else:
+            self.status = True
+
+    def check_http(self, secure: bool = False):
+        try:
+            if secure:
+                check_request = requests.get(f'https://{self.address}:{self.port}{self.route}', timeout=10)
+            else:
+                check_request = requests.get(f'http://{self.address}:{self.port}{self.route}', timeout=10)
+        except Exception as e:
+            logging.error(e)
+            return False
+        
+        # print(self.name, check_request.status_code)
+        # print(check_request.url)
+        if check_request.status_code < 400:
+            return True
+        else:
+            return False
+
+    def check_state(self):
+        match self.protocol:
+            case "http":
+                return self.check_http()
+            case "https":
+                return self.check_http(secure=True)
+            case _ :
+                return True
+
+    def __repr__(self):
+        return (f'Service name: {self.name} \n' +
+                f'Service address: {self.address} \n' +
+                f'Service protocol: {self.protocol} \n'+
+                f'Service port: {self.port}')
+    
+def send_msg(current_service: Service, 
+             status="Dead"):
+    
+    send_msg = str(current_service) + f"\nStatus: {status}"
+
+    send_text = f'{PlainText(send_msg).to_markdown()}'
+
+    send_data = {"chat_id": CHAT_ID ,
+             "parse_mode": "MarkdownV2",
+             "text": send_text
+             }
+    
+    send_url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+    
+    try:
+        sended_request = requests.post(url=send_url, data=send_data)
+        sended_request.raise_for_status()
+    except Exception as e:
+        logging.warn("Ошибка в отправке.")
+        logging.warn(sended_request.text)
+    finally:
+        logging.info(f"Статус отправленного сообщения: {sended_request.status_code}")
+        logging.info(send_msg)
+    pass
+
+if __name__ == "__main__":
+
+    logging.warning("Application started!")
+
+    services = []
+
+    with open(CONFIG_FILENAME,'r+',encoding='utf-8') as f:
+        list_of_services = yaml.safe_load(f.read())
+    
+    for i in list_of_services:
+        
+        curr_service = Service(name = i['name'],
+                                address = i['address'],
+                                protocol = i['protocol'])
+        if 'port' in i:
+            curr_service.port = i['port']
+        
+        if 'route' in i: 
+            curr_service.route = i['route']
+    
+        services.append(curr_service)
+
+    while(True):
+        for i in services:
+            if not i.check_state():
+                logging.warning(f'{i.name} is not available!')
+                if i.status == 0:
+                    pass
+                else:
+                    i.change_status()
+            else:
+                if i.status == False:
+                    i.change_status()
+                    i.notified = False
+                    send_msg(i, "is up! ✅")
+                logging.info(f'{i.name} is available') 
+        
+        all = len(services)
+        available = 0
+        unavailable = 0
+        for i in services:
+            
+            if i.status == 1:
+                available += 1
+            else:
+                unavailable += 1
+
+            if i.status == 0 and i.notified == False:
+                send_msg(i, "is DOWN! ❌")
+                i.notified = True
+
+        logging.info(f'Total hosts: {all}. Available : {available}. Unavailable: {unavailable}.')
+
+        time.sleep(120)
